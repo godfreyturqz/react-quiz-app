2cde64eb7d36ed8b72fb9bb31879cac54f70efdf
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-10-13 12:14:24 +0800

    update

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -31,13 +31,11 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps> `
         height:40px;
         margin: 5px 0;
         background: ${({ correct, userClicked }) => 
-            {
-        return correct
+            correct
             ? 'linear-gradient(90deg, #56ffa4, #59bc86)'
             : !correct && userClicked
-                ? 'linear-gradient(90deg, #ff5656, #c16868)'
-                : 'linear-gradient(90deg, #56ccff, #6eafb4)'
-    }
+            ? 'linear-gradient(90deg, #ff5656, #c16868)'
+            : 'linear-gradient(90deg, #56ccff, #6eafb4)'
         }
         border: 3px solid #000;
         box-shadow: 1px 2px 0px rgba(0,0,0,0.1);
